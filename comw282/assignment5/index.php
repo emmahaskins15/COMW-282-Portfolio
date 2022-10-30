cfa6b924e597701d118e29b36cca80820e1ff972
Emma Haskins <emmahaskins15@gmail.com>
2022-10-30 16:27:38 -0400

    Updated CSS

--- comw282/assignment5/index.php
+++ comw282/assignment5/index.php
@@ -13,26 +13,54 @@ include 'functions.php';
     <div class="formContainer">
     <h3>Add a test score into the database:</h3>    
         <form action="index.php" method="POST">
-            Group:
-            <select id="groupID" name="groupID">
-                <option value="null">-----</option>
-                <option value="A">A</option>
-                <option value="B">B</option>
-                <option value="C">C</option>
-                <option value="D">D</option>
-                <option value="E">E</option>
-            </select>
-            <br>
-            <label for="math">Math:</label>
-            <input type="number" id="math" name="math">
-            <br>
-            <label for="reading">Reading:</label>
-            <input type="number" id="reading" name="reading">
-            <br>
-            <label for="writing">Writing:</label>
-            <input type="number" id="writing" name="writing">
-            <br>
-            <input type="submit" name="insert">
+
+            <div class="row">
+                <div class="col-25">Group:</div>
+                <div class="col-75">
+                    <select id="groupID" name="groupID">
+                        <option value="null">-----</option>
+                        <option value="A">A</option>
+                        <option value="B">B</option>
+                        <option value="C">C</option>
+                        <option value="D">D</option>
+                        <option value="E">E</option>
+                    </select>
+                </div>
+                <br>
+            </div>
+
+            <div class="row">
+                <div class="col-25">
+                    <label for="math">Math:</label>
+                </div>
+                <div class="col-75">
+                    <input type="number" id="math" name="math">
+                </div>
+                <br>
+            </div>
+
+            <div class="row">
+                <div class="col-25">
+                    <label for="reading">Reading:</label>
+                </div>
+                <div class="col-75">
+                    <input type="number" id="reading" name="reading">
+                </div>
+                <br>
+            </div>
+
+            <div class="row">
+                <div class="col-25">
+                    <label for="writing">Writing:</label>
+                </div>
+                <div class="col-75">
+                    <input type="number" id="writing" name="writing">
+                </div>
+                <br>
+            </div>
+            <div class="row">
+                <input type="submit" name="insert">
+            </div>
         </form>
         <a href="scores.php">Scores</a>
     </div>
